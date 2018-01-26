0f322d0059a67fdb5c9cc4052eb80f629a8bacd3
smallnest <smallnest@gmail.com>
2018-01-26 14:51:50 +0800

    add go stdlib rpc code

---
+++ gostdrpc/gostd_server.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"flag"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	_ "net/http/pprof"
+	"net/rpc"
+	"time"
+
+	codec "github.com/mars9/codec"
+)
+
+type Hello int
+
+func (t *Hello) Say(args *BenchmarkMessage, reply *BenchmarkMessage) error {
+	args.Field1 = "OK"
+	args.Field2 = 100
+	*reply = *args
+	if *delay > 0 {
+		time.Sleep(*delay)
+	}
+	return nil
+}
+
+var (
+	host       = flag.String("s", "127.0.0.1:8972", "listened ip and port")
+	cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+	delay      = flag.Duration("delay", 0, "delay to mock business processing")
+	debugAddr  = flag.String("d", "127.0.0.1:9981", "server ip and port")
+)
+
+func main() {
+	flag.Parse()
+
+	go func() {
+		log.Println(http.ListenAndServe(*debugAddr, nil))
+	}()
+
+	server := rpc.NewServer()
+	server.Register(new(Hello))
+
+	ln, err := net.Listen("tcp", *host)
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Print("rpc.Serve: accept:", err.Error())
+			return
+		}
+		go serveConn(server, conn)
+	}
+}
+
+func serveConn(server *rpc.Server, conn io.ReadWriteCloser) {
+	srv := codec.NewServerCodec(conn)
+	server.ServeCodec(srv)
+}

43a9780914690d41c6ea28834952895bc64d41d4
smallnest <smallnest@gmail.com>
2018-01-31 14:07:56 +0800

    add rpcx http bench

---
+++ rpcx_http/rpcx_server.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+	"time"
+
+	rlog "github.com/smallnest/rpcx/log"
+	"github.com/smallnest/rpcx/server"
+)
+
+type Hello int
+
+func (t *Hello) Say(ctx context.Context, args *BenchmarkMessage, reply *BenchmarkMessage) error {
+	args.Field1 = "OK"
+	args.Field2 = 100
+	*reply = *args
+	if *delay > 0 {
+		time.Sleep(*delay)
+	}
+	return nil
+}
+
+var (
+	host       = flag.String("s", "127.0.0.1:8972", "listened ip and port")
+	cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+	delay      = flag.Duration("delay", 0, "delay to mock business processing")
+	debugAddr  = flag.String("d", "127.0.0.1:9981", "server ip and port")
+)
+
+func main() {
+	flag.Parse()
+
+	server.UsePool = true
+
+	rlog.SetDummyLogger()
+
+	go func() {
+		log.Println(http.ListenAndServe(*debugAddr, nil))
+	}()
+
+	server := server.NewServer()
+	server.RegisterName("Hello", new(Hello), "")
+	server.Serve("tcp", *host)
+}
